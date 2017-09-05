44175045b8c24027a4f7ec6431c60b6da5783cf0
Martin Onyesom <m.onyesom@gmail.com>
2017-09-05 01:38:15 +0100

    Optimization

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,13 +6,14 @@ import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
+import renderEngine.*;
 import models.RawModel;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by root on 8/31/17.
@@ -32,8 +33,8 @@ public class MainGameLoop {
          * game logic
          */
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
+//        StaticShader shader = new StaticShader();
+//        Renderer renderer = new Renderer(shader);
 
         float[] vertices = {
                 -0.5f,0.5f,-0.5f,
@@ -118,30 +119,45 @@ public class MainGameLoop {
 
         RawModel model = OBJLoader.loadOBJModel("stall", loader);
 
-        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
-        ModelTexture texture = staticModel.getTexture();
+        TexturedModel cubeModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+
+        ModelTexture texture = cubeModel.getTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(1);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0,-1,-10),0,0,0,1);
+//        Entity entity = new Entity(cubeModel, new Vector3f(0,-1,-10),0,0,0,1);
 
         Light light = new Light(new Vector3f(0,0,0), new Vector3f(1,1,1));
 
         Camera camera = new Camera();
 
+        List<Entity> allCubes = new ArrayList<>();
+//        Random random = new Random();
+//
+//        for(int i = 0; i < 200; i++){
+//            float x = random.nextFloat() * 100 - 50;
+//            float y = random.nextFloat() * 100 - 50;
+//            float z = random.nextFloat() * - 300;
+////            allCubes.add(new Entity(cubeModel, new Vector3f(0,-1,-10),0,0,0,1);
+//            allCubes.add(new Entity(cubeModel, new Vector3f(x,y,z),random.nextFloat() * 180f,random.nextFloat() * 180f,0f,1f));
+//        }
+
+        allCubes.add(new Entity(cubeModel, new Vector3f(0,-1,-10),0,0,0,1));
+
+        MasterRenderer renderer = new MasterRenderer();
         while (!Display.isCloseRequested()){
-//            entity.increasePosition(0,0,-0.1f);
-            entity.increaseRotation(0,1,0);
             camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity, shader);
-            shader.stop();
+
+            for(Entity cube : allCubes){
+                renderer.processEntity(cube);
+            }
+            allCubes.get(0).increaseRotation(0,1,0);
+            renderer.render(light, camera);
+
             DisplayManager.updateDisplay();
         }
-        shader.cleanUp();
+
+        renderer.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplace();
     }
